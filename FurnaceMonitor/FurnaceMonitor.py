545b5c931c23928757a94ae18839241388b70055
Wade Lykkehoy <WadeLykkehoy@gmail.com>
2020-03-30 15:36:19 -0500

    checkpointing code

--- FurnaceMonitor/FurnaceMonitor.py
+++ FurnaceMonitor/FurnaceMonitor.py
@@ -11,29 +11,34 @@ import board
 import busio
 import adafruit_adxl34x
 
-#from azure.iot.device import IoTHubDeviceClient, Message
+from azure.iot.device import IoTHubDeviceClient, Message
 
 
 # Some constant values; change these to change behavior of the app
 READINGS_PER_SECOND = 20
 RUNNING_AVG_WINDOW_SIZE = 20
 IOT_HUB_CONNECTION_STRING = "HostName=SEIS744Project.azure-devices.net;DeviceId=RazPi;SharedAccessKey=4lJeOPKGVS086AlDY/l2l0RYH7bABXhRvpk1THe7j+w="
+MSG_TEMPLATE = '{{"ts":{ts_data},"vib":{vib_data:10.8f},"state":{state_data}}}'
 
 
-def main_loop(verbose)
+
+def main_loop(verbose):
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(IOT_HUB_CONNECTION_STRING)
+
+    # Initialize the accelerometer
+    i2c = busio.I2C(board.SCL, board.SDA)
+    accel = adafruit_adxl34x.ADXL345(i2c)
+    
+    # Load the AI model to determine (predict) furnace state
+    state_pred_model = pickle.load(open('tree_pipeline.pkl', 'rb')) 
+    
     # Calc the amount of time (sec) to sleep between readings
     sleep_time = 1.0 / READINGS_PER_SECOND
     
     # We will be doing a running average of the readings over a given window size
     running_avg_window = collections.deque(maxlen=RUNNING_AVG_WINDOW_SIZE)
-    
-    # Load the AI model to determine (predict) furnace state
-    state_pred_model = pickle.load(open('tree_pipeline.pkl', 'rb'))
-    
-    # Initialize the accelerometer
-    i2c = busio.I2C(board.SCL, board.SDA)
-    accel = adafruit_adxl34x.ADXL345(i2c)
-    
+        
     # Initilize prevoius reading variables, else will start off with undefined or zero deltas
     prev_x, prev_y, prev_z = accel.acceleration
     time.sleep(sleep_time)
@@ -42,6 +47,8 @@ def main_loop(verbose)
     #  up to the Azure IoT Hub
     try: 
         while True:
+            # Take an accelerometer reading
+            reading_time = time.time()
             new_x, new_y, new_z = accel.acceleration
             
             # Turn the accelerometer reading into an indicator of vibration
@@ -56,14 +63,19 @@ def main_loop(verbose)
             running_avg = statistics.mean(running_avg_window)
         
             # Use our AI model to determine (predict) the state
-            state = state_pred_model.predict(running_avg)
+            X = np.array([[running_avg]])
+            state = state_pred_model.predict(X)
+            #if (verbose):
+            #    print('{}, {:>10.6f}'.format(state[0], running_avg), flush=True)
         
+            # Format the data into JSON
+            msg_text = MSG_TEMPLATE.format(ts_data=reading_time, vib_data=running_avg, state_data=state[0])
             if (verbose):
-                print('{}, {:>10.6f}'.format(state, running_avg))
-        
-            # Push the data on up to the Azure IoT Hub
+                print(msg_text, flush=True)
             
-            
+            # Package the message up and send on to the Azure IoT Hub
+            msg = Message(msg_text)
+            client.send_message(msg)          
             
             # Get ready for next reading
             prev_x = new_x
@@ -73,10 +85,11 @@ def main_loop(verbose)
             time.sleep(sleep_time)
 
     except KeyboardInterrupt:
-        # Just fall back to the main 
+        # Just fall back to the main
+        print('... Ctrl-C detected ...', flush=True)
 
 
-if __name__ == '__main__':
+if (__name__ == '__main__'):
     print ( "FurnaceMonitor started...", flush=True )
     print ( "Press Ctrl-C to stop", flush=True )
     main_loop(verbose=True)
